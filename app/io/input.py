a9082f557b1df83f544f329a8d8ed86f5d5d02a6
Kateryna Bondar <kateryna.bondar@ukma.edu.ua>
2025-04-23 05:40:05 +0300

    empty functions

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def console_input():
+    pass
+
+def file_read():
+    pass
+
+def pandas_file_read():
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,8 @@
+def console_output():
+    pass
+
+def file_write():
+    pass
+
+def pandas_file_write():
+    pass
